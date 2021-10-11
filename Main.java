58e2388971b85577f11804a67db87fbca6c0db14
Emre <61523720+yilmazemre61@users.noreply.github.com>
2021-10-11 15:53:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FactorPrinter.printFactors(6);
+        System.out.println("****");
+        FactorPrinter.printFactors(32);
+        System.out.println("****");
+        FactorPrinter.printFactors(10);
+        System.out.println("****");
+        FactorPrinter.printFactors(-1);
+    }
+}
